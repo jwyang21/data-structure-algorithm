a203558533dc25baceed1080f4b9f12bef29c28b
Jeewon Yang <jwyang.ai@gmail.com>
2023-03-04 22:09:34 +0900

    Create 15_find_duplicate_subtrees.py

---
+++ maps_dictionaries/15_find_duplicate_subtrees.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/explore/learn/card/hash-table/185/hash_table_design_the_key/1127/
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        def traverse(node):
+            if node:
+                node_structure = f'({traverse(node.left)}){node.val}({traverse(node.right)})'
+                if node_structure not in list(cnt.keys()):
+                    cnt[node_structure] = 0
+                cnt[node_structure] += 1
+                if cnt[node_structure] == 2:
+                    duplicates.append(node)
+                return node_structure
+            else:
+                return ""
+
+        cnt = {}
+        duplicates = []
+        traverse(root)
+        return duplicates
