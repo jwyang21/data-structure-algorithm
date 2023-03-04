648b87ff6c4d30e66eac43b9d722f0b5237af56f
Jeewon Yang <jwyang.ai@gmail.com>
2023-03-04 19:23:04 +0900

    Create 4_single_number.py

---
+++ maps_dictionaries/4_single_number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1176/
+
+from collections import Counter
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        for k in list(cnt.keys()):
+            if cnt[k] == 1:
+                tmp = k
+                break
+        return tmp
