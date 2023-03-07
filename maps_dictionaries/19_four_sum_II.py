9eaec61c6f8b86a7cd06303044a0bf504e26d33f
Jeewon Yang <jwyang.ai@gmail.com>
2023-03-07 13:54:13 +0900

    Create 19_four_sum_II.py

---
+++ maps_dictionaries/19_four_sum_II.py
@@ -0,0 +1,22 @@
+# https://velog.io/@jsh5408/leetcode-python3-454.-4Sum-II
+import collections
+
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        listA = collections.defaultdict(int)
+        listB = collections.defaultdict(int)
+        
+        for i in A:
+            for j in B:
+                listA[i+j] += 1
+        for i in C:
+            for j in D:
+                listB[i+j] += 1
+                
+        count = 0
+        
+        for i in listA:
+            if -i in listB:
+                count += listA[i] * listB[-i]
+                    
+        return count
