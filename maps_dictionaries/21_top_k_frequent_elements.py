fd70237835c73da802acdeedb3945e3fd723ac7c
Jeewon Yang <jwyang.ai@gmail.com>
2023-03-07 14:22:42 +0900

    Create 21_top_k_frequent_elements.py

---
+++ maps_dictionaries/21_top_k_frequent_elements.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/explore/learn/card/hash-table/187/conclusion-hash-table/1133/
+from collections import Counter
+import numpy as np
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt = Counter(nums)
+        keys = list(cnt.keys())
+        vals = [cnt[x] for x in keys]
+        max_indices = np.argpartition(vals, (-1)*k)[(-1)*k:]
+        return np.array(keys)[max_indices].tolist()
+
+        
